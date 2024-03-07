2d40e683e762a98774073db296b30892fef1a4b3
Abishek Anbazhagan <designer.abishek@gmail.com>
2024-03-08 01:53:12 +0530

    Added bg-color for Navbar and tried scroll event in js

--- script.js
+++ script.js
@@ -24,3 +24,12 @@ function addAnimation() {
     });
   });
 }
+
+// const navbar = document.querySelectorAll(".navbar");
+// const navbarLogo = document.getElementById("wca-nav-logo");
+
+// navbarLogo.addEventListener("scroll", myFunction);
+
+// function myFunction() {
+//   navbarLogo.style.height = "32px";
+// }
